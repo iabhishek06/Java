0dc2fae421b83268a6f1b8e893bc09b7dd4c1018
Abhishek <31386516+iabhishek06@users.noreply.github.com>
2024-01-25 21:30:53 +0530

    Add files via upload
    added synchronized block

--- DesignPattern/Singleton/Synchronized/Singleton3.java
+++ DesignPattern/Singleton/Synchronized/Singleton3.java
@@ -10,9 +10,18 @@ public class Singleton3 {
 
     public static Singleton3 getSingleton3(){
         if(singleton3 == null){
-            singleton3 = new Singleton3();
+            
+            // adding synchronized block because we dont want object to be created by multiple threads
+            // when multiple threds gets created they start to work concurrently, so if multiple
+            // threads get singleton3==null, they all will create object, so in case of multiple
+            // threads for security , we use synchronized block
+
+            synchronized (Singleton3.class) {
+                if (singleton3 == null) {
+                    singleton3 = new Singleton3();
+                }
+            }
         }
         return singleton3;
     }
 }
-

648cfd10bfab4cfeb6f9786040be60e21202cd39
Abhishek <31386516+iabhishek06@users.noreply.github.com>
2024-01-25 21:09:00 +0530

    Update Singleton3.java

--- DesignPattern/Singleton/Synchronized/Singleton3.java
+++ DesignPattern/Singleton/Synchronized/Singleton3.java
@@ -1,1 +1,18 @@
+package SingletonDP.Synchronized;
+
+public class Singleton3 {
+
+    private static Singleton3 singleton3;
+
+    private Singleton3(){
+
+    }
+
+    public static Singleton3 getSingleton3(){
+        if(singleton3 == null){
+            singleton3 = new Singleton3();
+        }
+        return singleton3;
+    }
+}
 
